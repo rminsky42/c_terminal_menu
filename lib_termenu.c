4deb4a802d296396cda9a5319276c2d5ef094be3
rminsky42 <rminsky42@gmail.com>
2025-07-23 19:11:52 -0700

    add create menu function

--- lib_termenu.c
+++ lib_termenu.c
@@ -5,4 +5,18 @@
 #include <string.h>
 #include "lib_termenu.h"
 
-
+Menu *create_menu(char *title, char **options, int count) {
+    Menu *menu = malloc(sizeof(Menu));
+    if (!menu) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    menu->title = strdup(title);
+    menu->options = malloc(count * sizeof(char *));
+    for (int i = 0; i < count; i++) {
+        menu->options[i] = strdup(options[i]);
+    }
+    menu->count = count;
+    menu->selected = 0;
+    return menu;
+}

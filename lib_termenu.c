c1b2a60191505badcee931ef1566d669bcd11f9e
rminsky42 <rminsky42@gmail.com>
2025-07-23 19:03:02 -0700

    Initial lib files

---
+++ lib_termenu.c
@@ -0,0 +1,8 @@
+//This library uses ANSI escape codes to create a terminal menu system.
+//// It provides functions to create menus, handle user input, and display options.
+
+#include <stdio.h>
+#include <string.h>
+#include "lib_termenu.h"
+
+
---
+++ lib_termenu.h
@@ -0,0 +1,16 @@
+#ifndef LIB_TERMENU_H
+#define LIB_TERMENU_H
+
+typedef struct {
+    char **options;      // Array of menu options
+    int count;          // Number of options
+    int selected;       // Index of the currently selected option
+    char *title;         // Title of the menu
+} Menu;
+
+Menu *create_menu(char *title, char **options, int count);
+void display_menu(Menu *menu);
+void handle_input(Menu *menu, char input);
+void free_menu(Menu *menu);
+
+#endif // LIB_TERMENU_H
